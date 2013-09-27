a5936bb61eadfc834e1881c153b82a37995f03f8
Justin Herrick <justin@justinherrick.com>
2013-09-27 14:03:05 -0400

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IosAcademyWeb::Application.config.secret_key_base = '8addb3a5eb711fb930825355b90663283d75839646748b39515c011b033aed458e5e843ca11bbd715534ff5e3694315aa0e17f8bde84344287dfed0cfa379cd6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IosAcademyWeb::Application.config.session_store :cookie_store, key: '_ios_academy_web_session'
